a894ed75fb009819a9245701f957c884e201ee25
simone.busoli <simone.busoli@535d68d8-ca53-0410-8573-011026200091>
2009-03-07 13:42:34 +0000

    CommonAssemblyInfo.cs file generated automatically with version information from the build server. Removed CommonAssemblyInfo.cs file from repository since it is created automatically. Added notice on how to build, before building in VS the nant build must be run. Added task to package the build artifacts, remains to configure the build server to publish the artifacts.

--- src/SharpArch/SharpArch.Data/Properties/AssemblyInfo.cs
+++ src/SharpArch/SharpArch.Data/Properties/AssemblyInfo.cs
@@ -1,25 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("SharpArch.Data")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Codai, LLC")]
 [assembly: AssemblyProduct("SharpArch.Data")]
-[assembly: AssemblyCopyright("Copyright © Codai, LLC 2008")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the Id of the typelib if this project is exposed to COM
-[assembly: Guid("a64b1b7f-dcfa-4d66-bd34-9bb104484d28")]
--- src/SharpArch/SharpArch.Testing.NUnit/Properties/AssemblyInfo.cs
+++ src/SharpArch/SharpArch.Testing.NUnit/Properties/AssemblyInfo.cs
@@ -1,25 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using System;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("SharpArch.Testing.NUnit")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Codai, LLC")]
 [assembly: AssemblyProduct("SharpArch.Testing.NUnit")]
-[assembly: AssemblyCopyright("Copyright © Codai, LLC 2008")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the Id of the typelib if this project is exposed to COM
-[assembly: Guid("4c553c66-055e-4c8f-9305-ad1b6ba07c61")]
