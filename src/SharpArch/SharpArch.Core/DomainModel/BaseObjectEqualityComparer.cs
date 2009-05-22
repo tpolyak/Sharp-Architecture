253014471e28b0e54a496f9aa99d7f309a7c94f5
wmccafferty <wmccafferty@535d68d8-ca53-0410-8573-011026200091>
2009-05-22 21:11:58 +0000

    Just added a little more commenting to describe that this may be used for comparing BaseObject objects or anything that derives from BaseObject.

--- src/SharpArch/SharpArch.Core/DomainModel/BaseObjectEqualityComparer.cs
+++ src/SharpArch/SharpArch.Core/DomainModel/BaseObjectEqualityComparer.cs
@@ -3,7 +3,9 @@
 namespace SharpArch.Core.DomainModel
 {
     /// <summary>
-    /// Provides a comparer for supporting LINQ methods such as Intersect, Union and Distinct
+    /// Provides a comparer for supporting LINQ methods such as Intersect, Union and Distinct.
+    /// This may be used for comparing objects of type <see cref="BaseObject" /> and anything 
+    /// that derives from it, such as <see cref="Entity" /> and <see cref="ValueObject" />.
     /// </summary>
     public class BaseObjectEqualityComparer : IEqualityComparer<BaseObject>
     {
