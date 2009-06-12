d73b0f509d712c65aac2a1e7f8fec80ce66308e3
frank.laub <frank.laub@535d68d8-ca53-0410-8573-011026200091>
2009-06-12 22:47:49 +0000

    Adding SharpArch.WcfClient.Castle.WcfSessionFacility

---
+++ src/SharpArch/SharpArch.WcfClient.Castle/WcfSessionFacility.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Castle.MicroKernel.ModelBuilder;
+using Castle.MicroKernel;
+using Castle.Core;
+using Castle.Core.Configuration;
+using System.ServiceModel;
+
+namespace SharpArch.WcfClient.Castle
+{
+	public class WcfSessionFacility : IFacility
+	{
+		public const string ManageWcfSessionsKey = "ManageWcfSessions";
+
+		#region IFacility Members
+
+		public void Init(IKernel kernel, IConfiguration facilityConfig) {
+			kernel.ComponentDestroyed += new ComponentInstanceDelegate(kernel_ComponentDestroyed);
+		}
+
+		void kernel_ComponentDestroyed(ComponentModel model, object instance) {
+			bool shouldManage = false;
+			object value = model.ExtendedProperties[ManageWcfSessionsKey];
+			if (value != null) {
+				shouldManage = (bool)value;
+			}
+			if (!shouldManage)
+				return;
+
+			ICommunicationObject obj = instance as ICommunicationObject;
+			if (obj != null) {
+				try {
+					if (obj.State != CommunicationState.Faulted) {
+						obj.Close();
+					}
+					else {
+						obj.Abort();
+					}
+				}
+				catch (CommunicationException) {
+					obj.Abort();
+				}
+				catch (System.TimeoutException) {
+					obj.Abort();
+				}
+				catch (Exception) {
+					obj.Abort();
+					throw;
+				}
+			}
+		}
+
+		public void Terminate() {
+		}
+
+		#endregion
+	}
+
+}
