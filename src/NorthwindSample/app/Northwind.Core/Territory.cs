9e15b5e6510738221e3604bc3c0a5095d8053005
Michael Aird <michael.aird@gmail.com>
2010-03-09 17:29:41 -0500

    Added a TypeConverter to allow re-binding the list of territories

--- src/NorthwindSample/app/Northwind.Core/Territory.cs
+++ src/NorthwindSample/app/Northwind.Core/Territory.cs
@@ -1,12 +1,16 @@
-﻿using SharpArch.Core.PersistenceSupport;
+﻿using System;
+using System.Globalization;
+using SharpArch.Core.PersistenceSupport;
 using SharpArch.Core.DomainModel;
 using System.Collections.Generic;
 using NHibernate.Validator.Constraints;
 using Northwind.Core.Organization;
 using SharpArch.Core;
+using System.ComponentModel;
 
 namespace Northwind.Core
 {
+    [TypeConverter(typeof(TerritoryConverter))]
     public class Territory : EntityWithTypedId<string>, IHasAssignedId<string>
     {
         public Territory() {
@@ -50,5 +54,31 @@ namespace Northwind.Core
         public virtual IList<Employee> Employees { get; protected set; }
 
         private const int ID_MAX_LENGTH = 20;
+
+        public class TerritoryConverter : TypeConverter
+        {
+            public override bool CanConvertFrom(ITypeDescriptorContext context,
+      Type sourceType)
+            {
+
+                if ( sourceType == typeof(string) )
+                {
+                    return true;
+                }
+                return base.CanConvertFrom(context, sourceType);
+            }
+
+            public override object ConvertFrom(ITypeDescriptorContext context,
+     CultureInfo culture, object value)
+            {
+                if ( value is string )
+                {
+                    Territory returnVal = new Territory();
+                    returnVal.SetAssignedIdTo(value as string);
+                    return returnVal;
+                }
+                return base.ConvertFrom(context, culture, value);
+            }
+        }
     }
 }
