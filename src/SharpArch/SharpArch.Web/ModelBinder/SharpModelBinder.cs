8ab34de8163951e2817012a571ab092eb9013fe2
Michael Aird <michael.aird@gmail.com>
2010-03-12 18:32:59 -0500

    Moving to the NHibernateValidatorProvider based on the work by Scott Kirkland (http://weblogs.asp.net/srkirkland/default.aspx)
    This removes a bunch of stuff in the ModelBinder and may mean we can do away
    with a couple of other classes as well.

--- src/SharpArch/SharpArch.Web/ModelBinder/SharpModelBinder.cs
+++ src/SharpArch/SharpArch.Web/ModelBinder/SharpModelBinder.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web.Mvc;
 using System.ComponentModel;
-using SharpArch.Core.CommonValidator;
-using SharpArch.Web.CommonValidator;
+using SharpArch.Core.CommonValidator; //TODO: Can we remove these classes now?
+using SharpArch.Web.CommonValidator; //TODO: Can we remove these classes now?
 using System;
 using SharpArch.Core.DomainModel;
 using System.Linq;
@@ -39,39 +39,6 @@ namespace SharpArch.Web.ModelBinder
             return base.OnModelUpdating(controllerContext, bindingContext);
         }
 
-        /// <summary>
-        /// After the model is updated, there may be a number of ModelState errors added by ASP.NET MVC for 
-        /// and data casting problems that it runs into while binding the object.  This gets rid of those
-        /// casting errors and uses the registered IValidator to populate the ModelState with any validation
-        /// errors.
-        /// </summary>
-        protected override void OnModelUpdated(ControllerContext controllerContext, ModelBindingContext bindingContext)
-        {
-            foreach (string key in bindingContext.ModelState.Keys)
-            {
-                for (int i = 0; i < bindingContext.ModelState[key].Errors.Count; i++)
-                {
-                    ModelError modelError = bindingContext.ModelState[key].Errors[i];
-
-                    // Get rid of all the MVC errors except those associated with parsing info; e.g., parsing DateTime fields
-                    if (IsModelErrorAddedByMvc(modelError) && !IsMvcModelBinderFormatException(modelError))
-                    {
-                        bindingContext.ModelState[key].Errors.RemoveAt(i);
-                        // Decrement the counter since we've shortened the list
-                        i--;
-                    }
-                }
-            }
-
-            // Transfer any errors exposed by IValidator to the ModelState
-            if (bindingContext.Model is IValidatable)
-            {
-                MvcValidationAdapter.TransferValidationMessagesTo(
-                    bindingContext.ModelName, bindingContext.ModelState,
-                    ((IValidatable)bindingContext.Model).ValidationResults());
-            }
-        }
-
         protected override object GetPropertyValue(ControllerContext controllerContext, ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, IModelBinder propertyBinder)
         {
             Type propertyType = propertyDescriptor.PropertyType;
@@ -96,17 +63,6 @@ namespace SharpArch.Web.ModelBinder
             return base.GetPropertyValue(controllerContext, bindingContext, propertyDescriptor, propertyBinder);
         }
 
-
-
-        /// <summary>
-        /// The base implementation of this uses IDataErrorInfo to check for validation errors and 
-        /// adds them to the ModelState. This override prevents that from occurring by doing nothing at all.
-        /// </summary>
-        protected override void OnPropertyValidated(ControllerContext controllerContext,
-            ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, object value)
-        {
-        }
-
         protected override void SetProperty(ControllerContext controllerContext,
             ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, object value)
         {
@@ -114,33 +70,6 @@ namespace SharpArch.Web.ModelBinder
             SetEntityCollectionProperty(bindingContext, propertyDescriptor, value);
 
             base.SetProperty(controllerContext, bindingContext, propertyDescriptor, value);
-        }
-
-        /// <summary>
-        /// The base implementatoin of this looks to see if a property value provided via a form is 
-        /// bindable to the property and adds an error to the ModelState if it's not.  For example, if 
-        /// a text box is left blank and the binding property is of type int, then the base implementation
-        /// will add an error with the message "A value is required." to the ModelState.  We don't want 
-        /// this to occur as we want these type of validation problems to be verified by our business rules.
-        /// </summary>
-        protected override bool OnPropertyValidating(ControllerContext controllerContext,
-            ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, object value)
-        {
-
-            return true;
-        }
-
-        private static bool IsModelErrorAddedByMvc(ModelError modelError)
-        {
-            return modelError.Exception != null &&
-                modelError.Exception.GetType().Equals(typeof(InvalidOperationException));
-        }
-
-        private static bool IsMvcModelBinderFormatException(ModelError modelError)
-        {
-            return modelError.Exception != null &&
-                modelError.Exception.InnerException != null &&
-                modelError.Exception.InnerException.GetType().Equals(typeof(FormatException));
         }
 
         private static bool IsEntityType(Type propertyType)
@@ -241,6 +170,24 @@ namespace SharpArch.Web.ModelBinder
     ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor)
         {
             base.BindProperty(controllerContext, bindingContext, propertyDescriptor);
+        }
+
+        protected override void OnPropertyValidated(ControllerContext controllerContext,
+            ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, object value)
+        {
+            base.OnPropertyValidated(controllerContext, bindingContext, propertyDescriptor,value);
+        }
+
+        protected override bool OnPropertyValidating(ControllerContext controllerContext,
+            ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, object value)
+        {
+
+            return base.OnPropertyValidating(controllerContext, bindingContext, propertyDescriptor, value);
+        }
+
+        protected override void OnModelUpdated(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            base.OnModelUpdated(controllerContext, bindingContext);
         }
         #endregion
 
