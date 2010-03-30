45a3461809eddf21056a7c087cdc21344691b380
Alec.Whittington <alec.whittington@gmail.com>
2010-03-29 17:00:47 -0700

    Final changes for RTM

--- src/NorthwindSample/app/Northwind.Web.Controllers/RouteRegistrar.cs
+++ src/NorthwindSample/app/Northwind.Web.Controllers/RouteRegistrar.cs
@@ -1,7 +1,5 @@
 ﻿using System.Web.Mvc;
 using System.Web.Routing;
-using SharpArch.Web.Areas;
-
 namespace Northwind.Web.Controllers
 {
     public class RouteRegistrar
@@ -10,22 +8,10 @@ namespace Northwind.Web.Controllers
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.IgnoreRoute("{*favicon}", new { favicon = @"(.*/)?favicon.ico(/.*)?" });
 
-            // The areas below must be registered from greater subareas to fewer;
-            // i.e., the root area should be the last area registered
-
-            // Example illustrative route with a nested area
-            //routes.CreateArea("Organization/Department", "Northwind.Web.Controllers.Organization.Department",
-            //    routes.MapRoute(null, "Organization/Department/{controller}/{action}", new { action = "Index" })
-            //);
-
-            routes.CreateArea("Organization", "Northwind.Web.Controllers.Organization",
-                routes.MapRoute(null, "Organization/{controller}/{action}", new { action = "Index" }),
-                routes.MapRoute(null, "Organization/{controller}/{action}/{id}")
-            );
-
-            // Routing config for the root area
-            routes.CreateArea("Root", "Northwind.Web.Controllers",
-                routes.MapRoute(null, "{controller}/{action}", new { controller = "Home", action = "Index" })
+            routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }  // Parameter defaults
             );
         }
     }
