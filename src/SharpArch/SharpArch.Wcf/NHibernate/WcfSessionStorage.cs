4d55899c037a7f133a1bf6d91fc34451b3c11171
sandord <sandor@subspace.nl>
2010-07-08 12:36:17 +0200

    Updated the WcfSessionStorage class to automatically fall back to either a default (SimpleSessionStorage) or a session storage implementation of choice whenever OperationContext.Current is null. This enables usage of NHibernate sessions in WCF host applications outside of WCF Operation methods without hassle.

--- src/SharpArch/SharpArch.Wcf/NHibernate/WcfSessionStorage.cs
+++ src/SharpArch/SharpArch.Wcf/NHibernate/WcfSessionStorage.cs
@@ -5,39 +5,80 @@ using System;
 
 namespace SharpArch.Wcf.NHibernate
 {
-	public class WcfSessionStorage : ISessionStorage
-	{
-		#region ISessionStorage Members
+    public class WcfSessionStorage : ISessionStorage
+    {
+        #region ISessionStorage Members
 
-		public ISession GetSessionForKey(string factoryKey)
-		{
-			SessionInstanceExtension instance = GetSessionInstanceExtension();
-			return instance.GetSessionForKey(factoryKey);
-		}
+        public ISession GetSessionForKey(string factoryKey)
+        {
+            if (OperationContext.Current == null)
+            {
+                return FallbackSessionStorage.GetSessionForKey(factoryKey);
+            }
 
-		public void SetSessionForKey(string factoryKey, ISession session)
-		{
-			SessionInstanceExtension instance = GetSessionInstanceExtension();
-			instance.SetSessionForKey(factoryKey, session);
-		}
+            SessionInstanceExtension instance = GetSessionInstanceExtension();
+            return instance.GetSessionForKey(factoryKey);
+        }
 
-		public System.Collections.Generic.IEnumerable<ISession> GetAllSessions()
-		{
-			SessionInstanceExtension instance = GetSessionInstanceExtension();
-			return instance.GetAllSessions();
-		}
+        public void SetSessionForKey(string factoryKey, ISession session)
+        {
+            if (OperationContext.Current == null)
+            {
+                FallbackSessionStorage.SetSessionForKey(factoryKey, session);
+                return;
+            }
 
-		#endregion
+            SessionInstanceExtension instance = GetSessionInstanceExtension();
+            instance.SetSessionForKey(factoryKey, session);
+        }
 
-		private SessionInstanceExtension GetSessionInstanceExtension()
-		{
-			SessionInstanceExtension instance =
-				OperationContext.Current.InstanceContext.Extensions.Find<SessionInstanceExtension>();
+        public System.Collections.Generic.IEnumerable<ISession> GetAllSessions()
+        {
+            if (OperationContext.Current == null)
+            {
+                return FallbackSessionStorage.GetAllSessions();
+            }
 
-			if (instance == null)
-				throw new Exception("SessionInstanceExtension not found in current OperationContext");
+            SessionInstanceExtension instance = GetSessionInstanceExtension();
+            return instance.GetAllSessions();
+        }
 
-			return instance;
-		}
-	}
+        #endregion
+
+        private ISessionStorage fallbackSessionStorage;
+
+        private ISessionStorage FallbackSessionStorage
+        {
+            get
+            {
+                return fallbackSessionStorage ?? (fallbackSessionStorage = new SimpleSessionStorage());
+            }
+
+            set
+            {
+                fallbackSessionStorage = value;
+            }
+        }
+
+        public WcfSessionStorage()
+            : this(null)
+        {
+        }
+
+        public WcfSessionStorage(ISessionStorage fallbackSessionStorage)
+        {
+            this.FallbackSessionStorage = fallbackSessionStorage;
+        }
+
+        private SessionInstanceExtension GetSessionInstanceExtension()
+        {
+            SessionInstanceExtension instance =
+                OperationContext.Current.InstanceContext.Extensions.Find<SessionInstanceExtension>();
+
+            if (instance == null)
+                throw new Exception("SessionInstanceExtension not found in current OperationContext");
+
+            return instance;
+        }
+    }
 }
