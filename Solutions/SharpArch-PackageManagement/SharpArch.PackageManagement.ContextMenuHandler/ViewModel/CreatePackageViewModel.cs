bbd90b9e05a618fc3745aa966676553d1d16ad0b
HowardvanRooijen <howardvanrooijen@gmail.com>
2010-08-08 21:39:32 +0100

    Add behaviour to ViewModels

--- Solutions/SharpArch-PackageManagement/SharpArch.PackageManagement.ContextMenuHandler/ViewModel/CreatePackageViewModel.cs
+++ Solutions/SharpArch-PackageManagement/SharpArch.PackageManagement.ContextMenuHandler/ViewModel/CreatePackageViewModel.cs
@@ -4,6 +4,7 @@ namespace SharpArch.PackageManagement.ContextMenuHandler.ViewModel
 
     using System;
     using System.ComponentModel.Composition;
+    using System.Windows;
 
     using Caliburn.Micro;
 
@@ -14,10 +15,74 @@ namespace SharpArch.PackageManagement.ContextMenuHandler.ViewModel
     [Export(typeof(ICreatePackageView))]
     public class CreatePackageViewModel : PropertyChangedBase, ICreatePackageView
     {
+        private string name;
+        private string author;
+        private string version;
+
+        public string Author
+        {
+            get
+            {
+                return this.author;
+            }
+
+            set
+            {
+                this.author = value;
+                this.NotifyOfPropertyChange(() => this.Author);
+                this.NotifyOfPropertyChange(() => this.CanCreatePackage);
+            }
+        }
+
+        public bool CanCreatePackage
+        {
+            get { return !string.IsNullOrWhiteSpace(this.Author) && !string.IsNullOrWhiteSpace(this.Name) && !string.IsNullOrWhiteSpace(this.Version); }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                this.name = value;
+                this.NotifyOfPropertyChange(() => this.Name);
+                this.NotifyOfPropertyChange(() => this.CanCreatePackage);
+            }
+        }
+
         public string Path
         {
             get;
             set;
+        }
+
+        public string Version
+        {
+            get
+            {
+                return this.version;
+            }
+
+            set
+            {
+                this.version = value;
+                this.NotifyOfPropertyChange(() => this.Version);
+                this.NotifyOfPropertyChange(() => this.CanCreatePackage);
+            }
+        }
+
+        public void CreatePackage()
+        {
+            MessageBox.Show(string.Format("Hello {0}!", this.Name));
+        }
+
+        public void Exit()
+        {
+            App.Current.Shutdown();
         }
     }
 }
--- Solutions/SharpArch-PackageManagement/SharpArch.PackageManagement.ContextMenuHandler/ViewModel/DeployPackageViewModel.cs
+++ Solutions/SharpArch-PackageManagement/SharpArch.PackageManagement.ContextMenuHandler/ViewModel/DeployPackageViewModel.cs
@@ -3,6 +3,7 @@ namespace SharpArch.PackageManagement.ContextMenuHandler.ViewModel
     #region Using Directives
 
     using System.ComponentModel.Composition;
+    using System.Windows;
 
     using Caliburn.Micro;
 
@@ -13,10 +14,42 @@ namespace SharpArch.PackageManagement.ContextMenuHandler.ViewModel
     [Export(typeof(IDeployPackageView))]
     public class DeployPackageViewModel : PropertyChangedBase, IDeployPackageView
     {
+        private string name;
+
         public string Path
         {
             get;
             set;
+        }
+
+        public bool CanDeployPackage
+        {
+            get { return !string.IsNullOrWhiteSpace(this.Name); }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                this.name = value;
+                this.NotifyOfPropertyChange(() => this.Name);
+                this.NotifyOfPropertyChange(() => this.CanDeployPackage);
+            }
+        }
+
+        public void DeployPackage()
+        {
+            MessageBox.Show(string.Format("Deploy {0}!", this.Name));
+        }
+
+        public void Exit()
+        {
+            App.Current.Shutdown();
         }
     }
 }
