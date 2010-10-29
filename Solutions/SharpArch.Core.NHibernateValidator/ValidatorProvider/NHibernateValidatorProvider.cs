2c8e1d02804cb4ce42908d898a2459afe4981126
Alec.Whittington <alec.whittington@gmail.com>
2010-10-28 20:33:19 -0700

    Added NHibernate.Validator client validation as provided by skirkland http://github.com/codai/Sharp-Architecture/pull/51

--- Solutions/SharpArch.Core.NHibernateValidator/ValidatorProvider/NHibernateValidatorProvider.cs
+++ Solutions/SharpArch.Core.NHibernateValidator/ValidatorProvider/NHibernateValidatorProvider.cs
@@ -1,48 +1,19 @@
 ﻿namespace SharpArch.Core.NHibernateValidator.ValidatorProvider
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
     using System.Web.Mvc;
 
-    using NHibernate.Validator.Constraints;
-    using NHibernate.Validator.Engine;
-
     /// <summary>
-    ///     Server side validator provider for NHVal
+    /// Server side validator provider for NHVal
     /// </summary>
-    public class NHibernateValidatorProvider : AssociatedValidatorProvider
+    public class NHibernateValidatorProvider : ModelValidatorProvider
     {
-        private readonly RuleEmitterList<IRuleArgs> ruleEmitters;
-
         /// <summary>
-        ///     ctor: Hook up the mappings between your attributes and model client validation rules
+        /// Returns model validators for each class that can be validated.
+        /// When this method is called with a non-class modelType, nothing is added to the yield return
+        /// (this prevents us from validating the same properties several times)
         /// </summary>
-        public NHibernateValidatorProvider()
-        {
-            this.ruleEmitters = new RuleEmitterList<IRuleArgs>();
-
-            this.ruleEmitters.AddSingle<NotNullNotEmptyAttribute>(
-                x => new ModelClientValidationRequiredRule(x.Message));
-            this.ruleEmitters.AddSingle<NotEmptyAttribute>(x => new ModelClientValidationRequiredRule(x.Message));
-            this.ruleEmitters.AddSingle<NotNullAttribute>(x => new ModelClientValidationRequiredRule(x.Message));
-
-            this.ruleEmitters.AddSingle<LengthAttribute>(
-                x => new ModelClientValidationStringLengthRule(x.Message, x.Min, x.Max));
-
-            this.ruleEmitters.AddSingle<MinAttribute>(
-                x => new ModelClientValidationRangeRule(x.Message, x.Value, null));
-            this.ruleEmitters.AddSingle<MaxAttribute>(
-                x => new ModelClientValidationRangeRule(x.Message, null, x.Value));
-
-            this.ruleEmitters.AddSingle<RangeAttribute>(
-                x => new ModelClientValidationRangeRule(x.Message, x.Min, x.Max));
-
-            this.ruleEmitters.AddSingle<PatternAttribute>(x => new ModelClientValidationRegexRule(x.Message, x.Regex));
-        }
-
-        protected override IEnumerable<ModelValidator> GetValidators(
-            ModelMetadata metadata, ControllerContext context, IEnumerable<Attribute> attributes)
+        public override IEnumerable<ModelValidator> GetValidators(ModelMetadata metadata, ControllerContext context)
         {
             var validationEngine = ValidatorEngineFactory.ValidatorEngine;
 
@@ -52,42 +23,6 @@
             {
                 yield return new NHibernateValidatorModelValidator(metadata, context, classValidator);
             }
-
-            if (metadata.ContainerType == null)
-            {
-                yield break; // Break if there is no metadata container
-            }
-
-            var validator = validationEngine.GetClassValidator(metadata.ContainerType);
-            if (validator == null)
-            {
-                yield break;
-            }
-
-            var constraints = validator.GetMemberConstraints(metadata.PropertyName).OfType<IRuleArgs>();
-
-            var rules = new List<ModelClientValidationRule>();
-
-            foreach (var constraint in constraints)
-            {
-                // for each constraint, emit the rules for that constraint
-                foreach (var validationRule in this.ruleEmitters.EmitRules(constraint))
-                {
-                    if (validationRule != null)
-                    {
-                        validationRule.ErrorMessage = constraint.Message; // TODO: If specified
-                    }
-
-                    rules.Add(validationRule);
-                }
-            }
-
-            if (rules.Count == 0)
-            {
-                yield break;
-            }
-
-            yield return new NHibernateValidatorClientValidator(metadata, context, rules);
         }
     }
 }
