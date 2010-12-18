9e5e8738b60f91841fed9bb0fdbc83fa77d31c13
Alec.Whittington <alec.whittington@gmail.com>
2010-12-18 12:09:29 -0700

    Updated WindsorExtensions to use latest Castle API

--- src/SharpArch/SharpArch.Web.Castle/WindsorExtensions.cs
+++ src/SharpArch/SharpArch.Web.Castle/WindsorExtensions.cs
@@ -1,10 +1,6 @@
 ﻿using Castle.MicroKernel.Registration;
 using System;
 using System.Linq;
-using Castle.Windsor;
-using System.Reflection;
-using System.Web.Mvc;
-using Castle.Core;
 
 namespace SharpArch.Web.Castle
 {
@@ -15,17 +11,22 @@ namespace SharpArch.Web.Castle
         /// <see cref="ServiceDescriptor" /> which is not generic and which 
         /// is found in the specified namespace.
         /// </summary>
-        public static BasedOnDescriptor FirstNonGenericCoreInterface(this ServiceDescriptor descriptor, string interfaceNamespace) {
-            return descriptor.Select(delegate(Type type, Type baseType) {
-                var interfaces = type.GetInterfaces()
-                    .Where(t => t.IsGenericType == false && t.Namespace.StartsWith(interfaceNamespace));
-                
-                if (interfaces.Count() > 0) {
-                    return new[] { interfaces.ElementAt(0) };
-                }
-                
-                return null;
-            });
+        public static BasedOnDescriptor FirstNonGenericCoreInterface(this ServiceDescriptor descriptor, string interfaceNamespace)
+        {
+            return descriptor.Select(
+                delegate(Type type, Type[] baseType)
+                {
+                    var interfaces =
+                        type.GetInterfaces().Where(
+                            t => t.IsGenericType == false && t.Namespace.StartsWith(interfaceNamespace));
+
+                    if (interfaces.Count() > 0)
+                    {
+                        return new[] { interfaces.ElementAt(0) };
+                    }
+
+                    return null;
+                });
         }
     }
 }
