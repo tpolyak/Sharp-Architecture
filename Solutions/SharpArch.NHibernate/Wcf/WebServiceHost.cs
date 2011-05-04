3ae5af7f6f99db6bdf04725a193ffd521150900d
geoffreysmith <smith.geoffrey.d@gmail.com>
2011-05-04 13:35:39 -0500

    Support for WCF 4 Web HTTP Services

---
+++ Solutions/SharpArch.NHibernate/Wcf/WebServiceHost.cs
@@ -0,0 +1,17 @@
+﻿namespace SharpArch.NHibernate.Wcf
+{
+    using System;
+
+    public class WebServiceHost : System.ServiceModel.Web.WebServiceHost
+    {
+        public WebServiceHost(Type serviceType, params Uri[] baseAddresses) : base(serviceType, baseAddresses)
+        {
+        }
+
+        protected override void OnOpening()
+        {
+            Description.Behaviors.Add(new ServiceBehavior());
+            base.OnOpening();
+        }
+    }
+}
