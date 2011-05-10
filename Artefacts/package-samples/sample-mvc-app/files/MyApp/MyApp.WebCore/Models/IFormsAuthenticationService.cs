e9bce00aad5e030d6690d22ff1b1933b438fbdb5
gs <smith.geoffrey.d@gmail.com>
2011-05-10 18:18:12 -0500

    Attempt to merge develop with master.

--- Artefacts/package-samples/sample-mvc-app/files/MyApp/MyApp.WebCore/Models/IFormsAuthenticationService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyApp.WebView.Models
-{
-    public interface IFormsAuthenticationService
-    {
-        void SignIn(string userName, bool createPersistentCookie);
-        void SignOut();
-    }
-}
