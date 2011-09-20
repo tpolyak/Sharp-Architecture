ed899895c98bbf15aa28791dc00ff02f7da89eb5
Zeke Lv <lvzecai@gmail.com>
2011-09-20 11:39:25 +0800

    Allow the Id field of a form model to be nullable

--- Solutions/SharpArch.Web.Mvc/ModelBinder/SharpModelBinder.cs
+++ Solutions/SharpArch.Web.Mvc/ModelBinder/SharpModelBinder.cs
@@ -149,7 +149,18 @@ namespace SharpArch.Web.Mvc.ModelBinder
             }
             else
             {
-                typedId = Convert.ChangeType(value, idType);
+                //handle nullable type
+                //http://stackoverflow.com/questions/793714/how-can-i-fix-this-up-to-do-generic-conversion-to-nullablet
+                Type underlyingType = Nullable.GetUnderlyingType(idType);
+
+                if (underlyingType == null)
+                {
+                    typedId = Convert.ChangeType(value, idType);
+                }
+                else
+                {
+                    typedId = Convert.ChangeType(value, underlyingType);
+                }
             }
 
             // First, look to see if there's an Id property declared on the entity itself;
