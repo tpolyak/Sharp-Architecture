355dbc95c4295d1dc73de8773469073437ccc67a
Seif Attar <iam@seifattar.net>
2011-10-16 11:29:33 +0100

    Reintroduce Fallback if Wcf extension not available
    Code was removed with the merge from develop and project restructure
    
    original commit by sandord:
    sharparchitecture/Sharp-Architecture@4d55899c037a7f133a1bf6d91fc34451b3c11171

--- Solutions/SharpArch.NHibernate/Wcf/WcfSessionStorage.cs
+++ Solutions/SharpArch.NHibernate/Wcf/WcfSessionStorage.cs
@@ -3,34 +3,76 @@ namespace SharpArch.NHibernate.Wcf
     using System;
     using System.Collections.Generic;
     using System.ServiceModel;
-
+    
     using global::NHibernate;
-
-    using NHibernate;
 
     public class WcfSessionStorage : ISessionStorage
     {
+        private ISessionStorage fallbackSessionStorage;
+
+        public WcfSessionStorage()
+            : this(null)
+        {
+        }
+
+        public WcfSessionStorage(ISessionStorage fallbackSessionStorage)
+        {
+            this.FallbackSessionStorage = fallbackSessionStorage;
+        }
+
+        private ISessionStorage FallbackSessionStorage
+        {
+            get
+            {
+                return this.fallbackSessionStorage ?? (this.fallbackSessionStorage = new SimpleSessionStorage());
+            }
+
+            set
+            {
+                this.fallbackSessionStorage = value;
+            }
+        }
+
+        #region ISessionStorage Members
         public IEnumerable<ISession> GetAllSessions()
         {
-            var instance = GetSessionInstanceExtension();
+            if (OperationContext.Current == null)
+            {
+                return this.FallbackSessionStorage.GetAllSessions();
+            }
+
+            SessionInstanceExtension instance = GetSessionInstanceExtension();
             return instance.GetAllSessions();
         }
 
         public ISession GetSessionForKey(string factoryKey)
         {
-            var instance = GetSessionInstanceExtension();
+            if (OperationContext.Current == null)
+            {
+                return this.FallbackSessionStorage.GetSessionForKey(factoryKey);
+            }
+
+            SessionInstanceExtension instance = GetSessionInstanceExtension();
             return instance.GetSessionForKey(factoryKey);
         }
 
         public void SetSessionForKey(string factoryKey, ISession session)
         {
-            var instance = GetSessionInstanceExtension();
+            if (OperationContext.Current == null)
+            {
+                this.FallbackSessionStorage.SetSessionForKey(factoryKey, session);
+                return;
+            }
+
+            SessionInstanceExtension instance = GetSessionInstanceExtension();
             instance.SetSessionForKey(factoryKey, session);
         }
+        #endregion
 
-        private static SessionInstanceExtension GetSessionInstanceExtension()
+        private SessionInstanceExtension GetSessionInstanceExtension()
         {
-            var instance = OperationContext.Current.InstanceContext.Extensions.Find<SessionInstanceExtension>();
+            SessionInstanceExtension instance =
+                OperationContext.Current.InstanceContext.Extensions.Find<SessionInstanceExtension>();
 
             if (instance == null)
             {
