208888ec5338336ccf73d456e92e5a5d3a175e6d
Seif Attar <iam@seifattar.net>
2012-02-04 12:38:31 +0000

    Add LinqRepositoryWithTypedId

--- Solutions/SharpArch.NHibernate/LinqRepository.cs
+++ Solutions/SharpArch.NHibernate/LinqRepository.cs
@@ -1,61 +1,6 @@
 ﻿namespace SharpArch.NHibernate
 {
-    using System.Linq;
-
-    using Domain.PersistenceSupport;
-    using Domain.Specifications;
-    using global::NHibernate.Linq;
-
-    public class LinqRepository<T> : NHibernateRepository<T>, ILinqRepository<T>
+    public class LinqRepository<T> : LinqRepositoryWithTypedId<T, int>
     {
-        public override void Delete(T target)
-        {
-            this.Session.Delete(target);
-        }
-
-        public IQueryable<T> FindAll()
-        {
-            return this.Session.Query<T>();
-        }
-
-        public IQueryable<T> FindAll(ILinqSpecification<T> specification)
-        {
-            return specification.SatisfyingElementsFrom(this.Session.Query<T>());
-        }
-
-        public T FindOne(int id)
-        {
-            return this.Session.Get<T>(id);
-        }
-
-        public T FindOne(ILinqSpecification<T> specification)
-        {
-            return specification.SatisfyingElementsFrom(this.Session.Query<T>()).SingleOrDefault();
-        }
-
-        public void Save(T entity)
-        {
-            try
-            {
-                this.Session.Save(entity);
-            }
-            catch
-            {
-                if (this.Session.IsOpen)
-                {
-                    this.Session.Close();
-                }
-
-                throw;
-            }
-
-            this.Session.Flush();
-        }
-
-        public void SaveAndEvict(T entity)
-        {
-            this.Save(entity);
-            this.Session.Evict(entity);
-        }
     }
 }
