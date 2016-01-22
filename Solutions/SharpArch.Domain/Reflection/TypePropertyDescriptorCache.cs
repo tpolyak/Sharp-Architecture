16af15afe075adb147a25e4faf61969850693d06
vk <vk@alphacloud.net>
2016-01-22 13:24:26 -0500

    WindsorPropertyInjectionExtensions - performance improvements, closes #137
    * Perform dependency validation only once.
    * Add CleanupInjectableProperties()

--- Solutions/SharpArch.Domain/Reflection/TypePropertyDescriptorCache.cs
+++ Solutions/SharpArch.Domain/Reflection/TypePropertyDescriptorCache.cs
@@ -12,6 +12,13 @@
         private readonly ConcurrentDictionary<Type, TypePropertyDescriptor> cache =
             new ConcurrentDictionary<Type, TypePropertyDescriptor>();
 
+        /// <summary>
+        ///     Find cached property descriptor.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <returns>
+        ///     <see cref="TypePropertyDescriptor" /> or <c>null</c> if does not exists.
+        /// </returns>
         public TypePropertyDescriptor Find(Type type)
         {
             TypePropertyDescriptor result;
@@ -19,15 +26,29 @@
             return result;
         }
 
+        /// <summary>
+        ///     Get existing property descriptor or create and cache it.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="factory">The factory to create descriptor.</param>
+        /// <returns></returns>
         public TypePropertyDescriptor GetOrAdd(Type type, Func<Type, TypePropertyDescriptor> factory)
         {
             Check.Require(factory != null, "Value factory can not be null.");
             return this.cache.GetOrAdd(type, factory);
         }
 
+        /// <summary>
+        ///     Clears the cache.
+        /// </summary>
         public void Clear()
         {
             this.cache.Clear();
         }
+
+        /// <summary>
+        ///     Returns number of entries in the cache.
+        /// </summary>
+        public int Count => this.cache.Count;
     }
 }
--- Solutions/SharpArch.Web.Mvc.Castle/WindsorPropertyInjectionExtensions.cs
+++ Solutions/SharpArch.Web.Mvc.Castle/WindsorPropertyInjectionExtensions.cs
@@ -7,17 +7,22 @@ namespace SharpArch.Castle.Extensions
     using System;
     using System.Collections.Generic;
     using System.Reflection;
+    using System.Runtime.CompilerServices;
     using global::Castle.Core;
     using global::Castle.MicroKernel;
     using global::Castle.MicroKernel.ComponentActivator;
     using SharpArch.Domain.Reflection;
 
+    /// <summary>
+    ///     Property injection support for Windsor.
+    /// </summary>
     internal static class WindsorPropertyInjectionExtensions
     {
+
         /// <summary>
         ///     Injects dependencies into properties.
         /// </summary>
-        /// <param name="kernel"></param>
+        /// <param name="kernel">Windsor kernel</param>
         /// <param name="target">The target object to inject properties.</param>
         /// <param name="cache">Injectable property descriptor cache.</param>
         /// <param name="validatePropertyRegistration">
@@ -29,64 +34,102 @@ namespace SharpArch.Castle.Extensions
         public static void InjectProperties(this IKernel kernel, object target, ITypePropertyDescriptorCache cache,
             Action<PropertyInfo, ComponentModel> validatePropertyRegistration = null)
         {
+            if (kernel == null)
+                throw new ArgumentNullException(nameof(kernel));
             if (target == null)
-            {
-                throw new ArgumentNullException("target");
-            }
+                throw new ArgumentNullException(nameof(target));
 
             Type type = target.GetType();
-
-            // Cache miss expected only once per given type, so call Find() first to prevent extra closure allocation in GetOrAdd.
-            TypePropertyDescriptor info = cache != null
-                ? cache.Find(type) ?? GetOrAdd(kernel, cache, type)
-                : GetInjectableProperties(type, kernel);
-
+            TypePropertyDescriptor info = GetInjectableProperties(kernel, cache, validatePropertyRegistration, type);
 
             if (!info.HasProperties())
-            {
                 return;
-            }
 
             for (var i = 0; i < info.Properties.Length; i++)
             {
                 PropertyInfo injectableProperty = info.Properties[i];
-                if (validatePropertyRegistration != null)
-                {
-                    IHandler registration = kernel.GetHandler(injectableProperty.PropertyType);
-                    if (registration != null)
-                    {
-                        validatePropertyRegistration(injectableProperty, registration.ComponentModel);
-                    }
-                }
                 object val = kernel.Resolve(injectableProperty.PropertyType);
                 try
                 {
                     injectableProperty.SetValue(target, val, null);
                 }
-
                 catch (Exception ex)
                 {
-                    string message = string.Format(
-                        "Error injecting property {0} on type {1}, See inner exception for more information.",
-                        injectableProperty.Name, type.FullName);
-
+                    string message =
+                        $"Error injecting property {injectableProperty.Name} on type {type.FullName}, See inner exception for more information.";
                     throw new ComponentActivatorException(message, ex, null);
                 }
             }
         }
 
-        static TypePropertyDescriptor GetOrAdd(IKernel kernel, ITypePropertyDescriptorCache cache, Type type)
+
+        /// <summary>
+        ///     Cleanups up injectable properties. All injectable properties will be set to <c>null</c>.
+        /// </summary>
+        /// <remarks>
+        ///     Since this method does not perform dependency validation
+        ///     <see cref="InjectProperties" /> , <paramref name="cache" /> is used as read-only.
+        ///     Calling <see cref="CleanupInjectableProperties" /> without call to <see cref="InjectProperties" /> on the same type
+        ///     will incur performance penalty as list of properties will be evaluated every time.
+        /// </remarks>
+        /// <param name="kernel">Windsor kernel.</param>
+        /// <param name="target">The target object to cleanup injectable properties.</param>
+        /// <param name="cache">Injectable property descriptor cache.</param>
+        public static void CleanupInjectableProperties(this IKernel kernel, object target,
+            ITypePropertyDescriptorCache cache)
         {
-            return cache.GetOrAdd(type, t => GetInjectableProperties(t, kernel));
+            if (kernel == null)
+                throw new ArgumentNullException(nameof(kernel));
+            if (target == null)
+                throw new ArgumentNullException(nameof(target));
+
+            Type type = target.GetType();
+            // Cache miss expected only once per given type, so call Find() first to prevent extra closure allocation in GetOrAdd.
+            TypePropertyDescriptor info = cache?.Find(type) ?? GetInjectableProperties(type, kernel, null);
+
+            if (!info.HasProperties())
+                return;
+
+            for (var i = 0; i < info.Properties.Length; i++)
+            {
+                info.Properties[i].SetValue(target, null, null);
+            }
         }
 
-        static TypePropertyDescriptor GetInjectableProperties(Type type, IKernel kernel)
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static TypePropertyDescriptor GetInjectableProperties(IKernel kernel, ITypePropertyDescriptorCache cache,
+            Action<PropertyInfo, ComponentModel> validatePropertyRegistration, Type type)
         {
-            var injectableProperties = new List<PropertyInfo>();
-            foreach (PropertyInfo prop in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            // Cache miss expected only once per given type, so call Find() first to prevent extra closure allocation in GetOrAdd.
+            TypePropertyDescriptor info = cache != null
+                ? cache.Find(type) ?? GetOrAdd(kernel, cache, type, validatePropertyRegistration)
+                : GetInjectableProperties(type, kernel, validatePropertyRegistration);
+            return info;
+        }
+
+        static TypePropertyDescriptor GetOrAdd(IKernel kernel, ITypePropertyDescriptorCache cache, Type type,
+            Action<PropertyInfo, ComponentModel> validatePropertyRegistration)
+        {
+            return cache.GetOrAdd(type, t => GetInjectableProperties(t, kernel, validatePropertyRegistration));
+        }
+
+        static TypePropertyDescriptor GetInjectableProperties(Type type, IKernel kernel,
+            Action<PropertyInfo, ComponentModel> validatePropertyRegistration)
+        {
+            PropertyInfo[] properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            if (properties.Length == 0)
+                return null;
+
+            var injectableProperties = new List<PropertyInfo>(properties.Length);
+            foreach (PropertyInfo prop in properties)
             {
-                if (prop.CanWrite && !prop.PropertyType.IsValueType && kernel.HasComponent(prop.PropertyType))
+                if (!prop.CanWrite || prop.PropertyType.IsValueType)
+                    continue;
+                IHandler registration = kernel.GetHandler(prop.PropertyType);
+                if (registration != null)
                 {
+                    validatePropertyRegistration?.Invoke(prop, registration.ComponentModel);
                     injectableProperties.Add(prop);
                 }
             }
